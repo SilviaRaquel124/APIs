f702f0b9aa0b3d390e7aee3654985747debcf7d3
Ricardo <ricardo@work.com>
2025-09-21 20:49:19 -0600

    Updated

--- v1/Grafica/grafica.php
+++ v1/Grafica/grafica.php
@@ -17,19 +17,28 @@ else {
 header("Content-Type: application/json");
 
 $sql = "SELECT 
-    (SELECT SUM(Monto) FROM entradas) +
-    (SELECT SUM(Monto) FROM salidas) AS suma_total;";
-    $result = $connection->query($sql); 
+   (SELECT COALESCE(SUM(Monto), 0) FROM entradas) AS total_entradas,
+    (SELECT COALESCE(SUM(Monto), 0) FROM salidas) AS total_salidas,
+    (SELECT COALESCE(SUM(Monto), 0) FROM entradas) +
+    (SELECT COALESCE(SUM(Monto), 0) FROM salidas) AS suma_total";
 
-    if($result)
-    {
-        $row = mysqli_fetch_assoc($result);
-        echo json_encode($row);
-    }
-    else
-    {
-        echo "No hay registros";
-    }
+$result = $connection->query($sql); 
+
+if($result && $result->num_rows > 0)
+{
+    $row = mysqli_fetch_assoc($result);
+    echo json_encode($row);
+}
+else
+{
+    echo json_encode(array(
+        "total_entradas" => 0,
+        "total_salidas" => 0,
+        "suma_total" => 0
+    ));
+}
+
+
 
 
 ?>
